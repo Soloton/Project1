e29beface97f27ff4706aa8068b0f9f1ff3af7fc
Mikhail Arkhipov <arkhipov-ms@ya.ru>
2020-10-02 22:50:37 +0300

    Практика урока 1

--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,7 @@
 fun main(args: Array<String>) {
-    print("Hello, Kotlin!")
-    print("Hello, Kotlin!")
-    print("Hello, Kotlin!")
-    println("Hello!")
-    println("Hello!")
-    println("Hello!")
+    println("Меня зовут Миша")
+    println(1598520)
+    println(true)
+    println(false)
+    println(45+18)
 }
