a9091196aece3d97334006762abf9a43b1abcbce
Mikhail Arkhipov <arkhipov-ms@ya.ru>
2020-10-02 22:49:11 +0300

    Урок 1

---
+++ src/Main.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    print("Hello, Kotlin!")
+    print("Hello, Kotlin!")
+    print("Hello, Kotlin!")
+    println("Hello!")
+    println("Hello!")
+    println("Hello!")
+}
